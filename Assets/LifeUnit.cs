908ce9093248777da632b3a7387421fc49f7940b
DESKTOP-09RIGVH\futou <futouyiba@gmail.com>
2022-01-22 23:57:12 +0800

    血豆循环动画。

--- Assets/LifeUnit.cs
+++ Assets/LifeUnit.cs
@@ -8,7 +8,7 @@ public class LifeUnit : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        GetComponent<SpriteRenderer>().DOFade(0.5f, 1f);
+        GetComponent<SpriteRenderer>().DOFade(0.5f, 3f).SetLoops(-1, LoopType.Yoyo);
     }
 
     // Update is called once per frame
