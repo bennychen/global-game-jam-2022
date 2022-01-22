c2b172a8441e106d7788815fc7f24a3da2acd110
Benny Chen <rockerbenny@gmail.com>
2022-01-22 20:43:52 +0800

    Refine dialogue bubble anim lifetime control

--- Assets/Scripts/Game/UI/NpcDialogueBubble.cs
+++ Assets/Scripts/Game/UI/NpcDialogueBubble.cs
@@ -5,24 +5,63 @@ public class NpcDialogueBubble : MonoBehaviour, Prime31.IObjectInspectable
 {
 	public UnityEngine.UI.Text text;
 
+	public bool IsAnimating
+	{
+		get
+		{
+			return _isAnimating;
+		}
+	}
+
 	public void Awake()
 	{
 		text = GetComponentInChildren<UnityEngine.UI.Text>();
 	}
 
+	public void PopupWithAnim(string txt, System.Action onComplete)
+	{
+		gameObject.SetActive(true);
+		this._onAnimComplete = onComplete;
+		StopAllCoroutines();
+		StartCoroutine(Typewriter(txt));
+	}
+
 	public void Popup(string txt)
 	{
-		StartCoroutine(Typewriter(txt));
+		StopAllCoroutines();
+		gameObject.SetActive(true);
+		text.text = txt;
+	}
+
+	public void Hide()
+	{
+		StopAllCoroutines();
+		gameObject.SetActive(false);
 	}
 
 	private IEnumerator Typewriter(string txt)
 	{
 		text.text = "";
+		_isAnimating = true;
 		foreach (char c in txt)
 		{
 			text.text = text.text + c;
-			yield return new WaitForSeconds(.03f);
+			if (_isAnimating)
+			{
+				yield return new WaitForSeconds(.03f);
+			}
+			else
+			{
+				text.text = txt;
+			}
+		}
+		if (this._onAnimComplete != null)
+		{
+			this._onAnimComplete();
 		}
 		yield return null;
 	}
+
+	private bool _isAnimating;
+	private System.Action _onAnimComplete;
 }
