92c99f1381e126a0be65422deecbd9658fc95bb0
DESKTOP-09RIGVH\futou <futouyiba@gmail.com>
2022-01-23 12:13:11 +0800

    now guide state would be jumped from when playing time exceeds 47 sec.

--- Assets/Scripts/Game/GameState/GuidState.cs
+++ Assets/Scripts/Game/GameState/GuidState.cs
@@ -1,20 +1,35 @@
-﻿
-using Codeplay;
+﻿using Codeplay;
+using UnityEngine;
 using UnityEngine.Video;
 
 namespace Game
 {
     public class GuidState : State<GameController>
     {
+        private VideoPlayer _videoPlayer;
+        private float _accuTime =0f;
+
         public override void OnEnter()
         {
             base.OnEnter();
             var contextGameGuideScene = _context.GameGuideScene;
             contextGameGuideScene.SetActive(true);
-            var videoPlayer = contextGameGuideScene.GetComponentInChildren<VideoPlayer>();
-            videoPlayer.Play();
-            // videoPlayer.
-            
+            _videoPlayer = contextGameGuideScene.GetComponentInChildren<VideoPlayer>();
+            _videoPlayer.Play();
+            // VideoPlayer.TimeEventHandler += JudgeTimeFinish;
+
+        }
+
+        public override void OnUpdate(float deltaTime)
+        {
+            base.OnUpdate(deltaTime);
+            _accuTime += deltaTime;
+            // Debug.Log(_accuTime);
+            if ((_accuTime>47f))
+            // if ((_accuTime>55f) && (!_videoPlayer.isPlaying))
+            {
+                _stateMachine.ChangeState<GameLoopState>();
+            }
         }
 
         public override void OnExit()
